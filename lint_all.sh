8aed48ae5cac2d515e46162deb074715e358c9d3
mitch-avis <mitchavis@gmail.com>
2024-06-02 06:05:27 -0600

    Add my files

---
+++ lint_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Sorting imports with isort..."
+isort .
+isort **/tests/*.py
+
+echo "Reformatting with black..."
+black .
+black **/tests/*.py
+
+echo "Linting with flake8..."
+flake8 .
+flake8 **/tests/*.py
+
+echo "Linting with pylint..."
+pylint nfl_data_py/
